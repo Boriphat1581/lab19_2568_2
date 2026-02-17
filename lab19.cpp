5d4319ed337afdde2739b3abbbae866a0ff3432f
Boriphat1581 <boriphat_palee@cmu.ac.th>
2026-02-17 16:24:01 +0700

    cpp19

--- lab19.cpp
+++ lab19.cpp
@@ -1,50 +1,34 @@
-// Please include header (lab21.h) here.
+#ifndef LAB21_H
+#define LAB21_H
 
-int main(){
-	srand(time(0));
-	Unit hero,mons;
-	hero.create("Hero");
-	mons.create("Monster");	
-	int turn_count = 1;
-	char player_action = '\0',monster_action = '\0';
-	int p = 0, m = 0;
-	while(true){
-		mons.newTurn();	
-		hero.newTurn();			
-		mons.showStatus();
-		drawScene(player_action,p,monster_action,m);
-		hero.showStatus();		
-		cout << "[A] Attack [H] Heal [G] Guard [E] Exit";
-		cout << "\n[Turn " << turn_count << "] Enter your action: ";
-		cin >> player_action;
-		player_action = toupper(player_action);
-		if(player_action == 'E') break; 
-		
-		int temp = rand()%3;
-		if(temp <= 1) monster_action = 'A';
-		else if(temp == 2) monster_action = 'G';
-		
-		if(player_action == 'G') hero.guard();
-		if(monster_action == 'G') mons.guard();
-		
-		if(player_action == 'H') p = hero.heal();
-		
-		if(player_action == 'A') p = hero.attack(mons); 
-		if(monster_action == 'A') m = mons.attack(hero); 
-		
-		if(hero.isDead()){
-			drawScene(player_action,p,monster_action,m);
-			playerLose();
-			break; 
-		}
-		
-		if(mons.isDead()){
-			drawScene(player_action,p,monster_action,m);
-			playerWin();
-			break; 
-		}
-		
-		turn_count++;
-	}
-	return 0;
-}
+#include<iostream>
+#include<string>
+#include<ctime>
+#include<cstdlib>
+#include<iomanip>
+using namespace std;
+
+class Unit{
+    string name;
+    string type;        
+    int hp;
+    int hpmax;
+    int atk;
+    int def;
+    bool guard_on;        
+public:            
+    void create(string);
+    void showStatus();
+    void newTurn();
+    int attack(Unit &);
+    int beAttacked(int);
+    int heal();    
+    void guard();
+    bool isDead();    
+};
+
+void drawScene(char p_action,int p,char m_action,int m);
+void playerWin();
+void playerLose();
+
+#endif
